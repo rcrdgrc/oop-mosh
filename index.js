a05c958ad04f9a22b9b438a70d700269eacc8d8e
rcrdgrc <ricktx04@gmail.com>
2020-02-19 20:20:16 -0600

    starte on objects

---
+++ index.js
@@ -0,0 +1,14 @@
+//objects in js are key value pairs 
+
+const circle = {
+    radius: 1,
+    loaction: {
+        x: 1,
+        y: 1,
+    },
+    draw: function() {
+        console.log('draw');
+    }
+}
+
+circle.draw();`~
